dd8ca5dc7aea2f007f44431af2b3e5ceec79d276
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-11-29 10:09:15 +0000

    feat: scaffold project

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A basic template for bevy projects";
+  inputs = {
+    nixpkgs.url = "nixpkgs/24.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    allSystems = ["x86_64-linux"];
+    overlays = [(import rust-overlay)];
+    forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {pkgs = import nixpkgs {inherit system overlays;};});
+  in {
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          pkg-config
+          lld
+          # rust
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          # For commitlint
+          nodejs_22
+          # Command runner
+          just
+        ];
+        shellHook = ''
+          npm install
+          npm run prepare
+        '';
+      };
+    });
+  };
+}
