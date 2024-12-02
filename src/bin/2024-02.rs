d9adb7dd936ee4ee4d0b59aea187d3f64f41bb19
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-02 11:18:37 +0000

    feat: 2024-02 star 1
    perf: 1.6ms

--- src/bin/2024-02.rs
+++ src/bin/2024-02.rs
@@ -1,1 +1,119 @@
-fn main() {}
+#[derive(PartialEq, Debug)]
+enum ReportStatus {
+    Rising,
+    Falling,
+    Unsafe,
+    Unset,
+}
+
+#[derive(Debug)]
+struct StateTracker {
+    state: ReportStatus,
+    prev: i32,
+}
+
+fn main() {
+    let actual_input = include_str!("../../puzzles/2024/02/input");
+    let test_input = "\
+        7 6 4 2 1\n\
+        1 2 7 8 9\n\
+        9 7 6 2 1\n\
+        1 3 2 4 5\n\
+        8 6 4 4 1\n\
+        1 3 6 7 9";
+    let using_actual_input = true;
+
+    let input = if using_actual_input {
+        actual_input
+    } else {
+        test_input
+    };
+
+    let reports = input
+        .split('\n')
+        .map(|report| {
+            report
+                .split(' ')
+                .filter(|num| num.trim() != "")
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<i32>>>();
+    // [["7", "6", ...], ...]
+    let report_status = reports
+        .iter()
+        .map(|report| {
+            report
+                .iter()
+                .fold(
+                    StateTracker {
+                        state: ReportStatus::Unset,
+                        prev: -1,
+                    },
+                    |state_tracker, current| {
+                        // dbg!(&state_tracker);
+                        if state_tracker.prev == -1 {
+                            StateTracker {
+                                state: ReportStatus::Unset,
+                                prev: *current,
+                            }
+                        } else {
+                            StateTracker {
+                                state: is_safe(state_tracker.state, state_tracker.prev, *current),
+                                prev: *current,
+                            }
+                        }
+                    },
+                )
+                .state
+            // != CurrentState::Unsafe
+        })
+        .filter(|report| *report != ReportStatus::Unset);
+    println!();
+
+    // dbg!(safe_reports.clone().collect::<Vec<&Vec<i32>>>());
+    dbg!(report_status.clone().collect::<Vec<ReportStatus>>());
+
+    let output_1 = report_status
+        .filter(|report| *report != ReportStatus::Unsafe)
+        .count();
+
+    dbg!(output_1);
+    assert_eq!(if using_actual_input { 230 } else { 2 }, output_1);
+}
+
+fn change_type(prev: i32, next: i32) -> ReportStatus {
+    if prev == next {
+        return ReportStatus::Unsafe;
+    }
+
+    if prev < next {
+        ReportStatus::Rising
+    } else {
+        ReportStatus::Falling
+    }
+}
+
+fn is_safe(state: ReportStatus, prev: i32, next: i32) -> ReportStatus {
+    if let ReportStatus::Unsafe = state {
+        return state;
+    }
+    // Change > 3
+    if (prev - next).abs() > 3 {
+        return ReportStatus::Unsafe;
+    }
+    // Change < 1
+    if prev == next {
+        return ReportStatus::Unsafe;
+    }
+
+    if let ReportStatus::Unset = state {
+        return change_type(prev, next);
+    }
+
+    if change_type(prev, next) == state {
+        state
+    } else {
+        ReportStatus::Unsafe
+    }
+}
