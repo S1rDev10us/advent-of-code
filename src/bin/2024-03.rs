da5b1e62717c4edc580eee2b7d1b4445a36df686
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-03 19:59:42 +0000

    feat: start 2024-03

---
+++ src/bin/2024-03.rs
@@ -0,0 +1,14 @@
+use regex::Regex;
+
+fn main() {
+    let actual_input = include_str!("../../puzzles/2024/03/input");
+    let test_input = "\
+        ";
+    let using_actual_input = true;
+
+    let input = if using_actual_input {
+        actual_input
+    } else {
+        test_input
+    };
+}
