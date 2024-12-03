54f8952d79fdb9d85a070bd26d93c140d9dc06a5
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-03 20:21:32 +0000

    feat: 2024-03 star 1
    perf: 2.4ms

--- src/bin/2024-03.rs
+++ src/bin/2024-03.rs
@@ -11,4 +11,17 @@ fn main() {
     } else {
         test_input
     };
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let pairs = re.captures_iter(input);
+
+    let output_1 = pairs
+        .map(|m| (m[1].to_string(), m[2].to_string()))
+        .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
+        .map(|(a, b)| a * b)
+        .sum::<i32>();
+
+    dbg!(output_1);
+    assert_eq!(161289189i32, output_1);
 }
