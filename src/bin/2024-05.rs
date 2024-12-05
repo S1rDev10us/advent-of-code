1da40e8f48af55ed59da7c8bb962db13a45118d5
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-05 12:35:30 +0000

    feat: 2024-05 star 2
    perf: 1.1ms

--- src/bin/2024-05.rs
+++ src/bin/2024-05.rs
@@ -1,8 +1,9 @@
 use advent_of_code::initialize_macro;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 
 fn main() {
-    let (input, _is_star_2, _is_actual_input) = initialize_macro!(
+    let (input, _, is_actual_input) = initialize_macro!(
         "2024/05",
         "\
             47|53\n\
@@ -56,38 +57,85 @@ fn main() {
             map
         });
 
-    dbg!(&rules);
+    // dbg!(&rules);
 
     let updates = sections[1]
         .split('\n')
         .filter(|update| update.trim() != "")
         .map(|update| update.split(','))
         .map(|update| update.collect::<Vec<_>>())
-        .filter(|update| {
+        .map(|update| {
+            let mut found = vec![];
             let mut blacklist = vec![];
-            update.iter().all(|item| {
-                dbg!(&blacklist, item);
-                if blacklist.contains(item) {
-                    return false;
-                }
-                if let Some(new_rules) = rules.get(item) {
-                    blacklist.append(&mut new_rules.clone());
-                }
-                true
-            })
-        });
+            (
+                update.iter().all(|item| {
+                    // dbg!(&blacklist, item);
+                    if blacklist.contains(item) {
+                        return false;
+                    }
+                    if !found.contains(&item) {
+                        if let Some(new_rules) = rules.get(item) {
+                            blacklist.append(&mut new_rules.clone());
+                        }
+                        found.push(item);
+                    }
+                    true
+                }),
+                update,
+            )
+        })
+        .collect::<Vec<_>>();
 
     let output_1 = updates
+        .iter()
+        .filter(|update| update.0)
+        .map(|update| &update.1)
         .map(|update| {
-            dbg!(&update);
+            // dbg!(&update);
             update[update.len() / 2].parse::<i32>().unwrap()
         })
         .sum::<i32>();
     dbg!(output_1);
 
-    if _is_actual_input {
+    if is_actual_input {
         assert_eq!(output_1, 4872);
     } else {
         assert_eq!(output_1, 143);
+    }
+
+    let output_2 = updates
+        .iter()
+        .filter(|update| !update.0)
+        .map(|update| update.1.clone())
+        .map(|mut update| {
+            update.sort_by(|a, b| {
+                if a == b {
+                    return Ordering::Equal;
+                }
+                if let Some(rule_list) = rules.get(a) {
+                    if rule_list.contains(b) {
+                        return Ordering::Less;
+                    }
+                }
+                if let Some(rule_list) = rules.get(b) {
+                    if rule_list.contains(a) {
+                        return Ordering::Greater;
+                    }
+                }
+                Ordering::Equal
+            });
+            update
+        })
+        .map(|update| {
+            // dbg!(&update);
+            update[update.len() / 2].parse::<i32>().unwrap()
+        })
+        .sum::<i32>();
+    dbg!(output_2);
+
+    if is_actual_input {
+        assert_eq!(output_2, 5564);
+    } else {
+        assert_eq!(output_2, 123);
     }
 }
