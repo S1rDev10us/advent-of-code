cdcba1041b91bc21759d54c1099fb61ff59b40f0
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-06 19:21:17 +0000

    feat: start 2024-06

---
+++ src/bin/2024-06.rs
@@ -0,0 +1,19 @@
+use advent_of_code::initialize_macro;
+
+fn main() {
+    let (input, _, is_actual_input) = initialize_macro!(
+        "2024/06",
+        "\
+            ....#.....\n\
+            .........#\n\
+            ..........\n\
+            ..#.......\n\
+            .......#..\n\
+            ..........\n\
+            .#..^.....\n\
+            ........#.\n\
+            #.........\n\
+            ......#...\
+        "
+    );
+}
