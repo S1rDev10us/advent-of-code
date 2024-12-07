8e2e74bc5c43c1f5fdca8b4e0ef362e49c89652d
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-07 01:10:58 +0000

    feat: 2024-06 star 2
    perf: 3.906s
    modified star 1 perf: 3.9ms

--- src/bin/2024-06.rs
+++ src/bin/2024-06.rs
@@ -1,7 +1,8 @@
 use advent_of_code::initialize_macro;
+use std::collections::HashSet;
 use std::fmt;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Right,
@@ -34,14 +35,16 @@ impl Direction {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Tile {
     Guard(Direction),
     Empty(bool),
     Obstruction,
 }
 
+#[derive(Debug, Clone)]
 struct Map(Vec<Vec<Tile>>);
+
 impl Map {
     fn pos_in_map(&self, pos: (isize, isize)) -> bool {
         pos.0 >= 0
@@ -49,11 +52,11 @@ impl Map {
             && self.0.len() > pos.1 as usize
             && self.0[pos.1 as usize].len() > pos.0 as usize
     }
-    fn get_pos(&self, pos: (isize, isize)) -> Option<&Tile> {
+    fn get_pos(&self, pos: (isize, isize)) -> Option<Tile> {
         if !self.pos_in_map(pos) {
             return None;
         }
-        Some(&self.0[pos.1 as usize][pos.0 as usize])
+        Some(self.0[pos.1 as usize][pos.0 as usize].clone())
     }
     fn set_pos(&mut self, pos: (isize, isize), tile: Tile) -> bool {
         if !self.pos_in_map(pos) {
@@ -63,21 +66,33 @@ impl Map {
         self.0[pos.1 as usize][pos.0 as usize] = tile;
         true
     }
-    fn guards(&self) -> Vec<(usize, usize)> {
-        let mut guards = vec![];
-        for (y, row) in self.0.iter().enumerate() {
-            for (x, tile) in row.iter().enumerate() {
-                if let Tile::Guard(_) = tile {
-                    guards.push((x, y));
-                }
-            }
-        }
-        guards
+}
+
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{:}",
+            self.0
+                .iter()
+                .map(|row| row
+                    .iter()
+                    .map(|tile| match tile {
+                        Tile::Empty(true) => String::from("X"),
+                        Tile::Empty(false) => String::from("."),
+                        Tile::Guard(dir) => format!("{}", dir),
+                        Tile::Obstruction => String::from("#"),
+                    })
+                    .collect::<Vec<_>>()
+                    .join(""))
+                .collect::<Vec<_>>()
+                .join("\n")
+        )
     }
 }
 
 fn main() {
-    let (input, _, is_actual_input) = initialize_macro!(
+    let (input, is_star_2, is_actual_input) = initialize_macro!(
         "2024/06",
         "\
             ....#.....\n\
@@ -108,50 +123,92 @@ fn main() {
         })
         .collect::<Vec<_>>());
 
-    loop {
-        let guards = map.guards();
-        if guards.is_empty() {
-            break;
-        }
-        for guard in guards {
-            move_guard(&mut map, guard);
-        }
-        if !is_actual_input {
-            println!(
-                "{:}\n",
-                map.0
-                    .iter()
-                    .map(|row| row
-                        .iter()
-                        .map(|tile| match tile {
-                            Tile::Empty(true) => String::from("X"),
-                            Tile::Empty(false) => String::from("."),
-                            Tile::Guard(dir) => format!("{}", dir),
-                            Tile::Obstruction => String::from("#"),
-                        })
-                        .collect::<Vec<_>>()
-                        .join(""))
-                    .collect::<Vec<_>>()
-                    .join("\n")
-            );
-        }
-    }
+    let base_evalled_map = eval_map(map.clone(), is_actual_input).0;
 
-    let output_1 = map
+    let walked_positions = base_evalled_map
         .0
         .iter()
-        .flatten()
-        .filter(|tile| matches!(tile, Tile::Empty(true)))
-        .count();
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(x, tile)| (matches!(tile, Tile::Empty(true)), (x, y)))
+                .filter(|(walked, _)| *walked)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let output_1 = walked_positions.len();
     dbg!(output_1);
     if is_actual_input {
         assert_eq!(4433, output_1);
     } else {
         assert_eq!(41, output_1);
     }
+
+    if is_star_2 {
+        let looping_maps = walked_positions
+            .iter()
+            .map(|(_, pos)| {
+                let mut map_with_obstruction = map.clone();
+                map_with_obstruction.set_pos((pos.0 as isize, pos.1 as isize), Tile::Obstruction);
+                eval_map(map_with_obstruction, is_actual_input)
+            })
+            .filter(|(_, did_loop)| *did_loop);
+
+        let output_2 = looping_maps.count();
+        dbg!(output_2);
+        if is_actual_input {
+            assert_eq!(1516, output_2);
+        } else {
+            assert_eq!(6, output_2);
+        }
+    }
 }
 
-fn move_guard(map: &mut Map, pos: (usize, usize)) {
+fn eval_map(mut map: Map, is_actual_input: bool) -> (Map, bool) {
+    let mut guard_pos: Option<(usize, usize)> = 'guard_pos: {
+        for (y, row) in map.0.iter().enumerate() {
+            for (x, tile) in row.iter().enumerate() {
+                if matches!(tile, Tile::Guard(_)) {
+                    break 'guard_pos Some((x, y));
+                }
+            }
+        }
+        None
+    };
+
+    let mut travelled_positions = HashSet::new();
+
+    while guard_pos.is_some() {
+        let pos = guard_pos.unwrap();
+
+        let guard = map.get_pos((pos.0 as isize, pos.1 as isize)).unwrap();
+
+        let dir = if let Tile::Guard(dir) = guard {
+            dir
+        } else {
+            unreachable!()
+        };
+
+        let pos_with_dir = (guard_pos, dir);
+
+        if travelled_positions.contains(&pos_with_dir) {
+            return (map, true);
+        }
+
+        travelled_positions.insert(pos_with_dir);
+
+        guard_pos = move_guard(&mut map, pos);
+
+        if !is_actual_input {
+            println!("{:}", map);
+        }
+    }
+    (map, false)
+}
+
+fn move_guard(map: &mut Map, pos: (usize, usize)) -> Option<(usize, usize)> {
     let ipos = (pos.0 as isize, pos.1 as isize);
 
     match map.get_pos(ipos) {
@@ -163,6 +220,7 @@ fn move_guard(map: &mut Map, pos: (usize, usize)) {
                     Tile::Empty(_) => {
                         map.set_pos(targeted_position, Tile::Guard(dir.clone()));
                         map.set_pos(ipos, Tile::Empty(true));
+                        return Some((targeted_position.0 as usize, targeted_position.1 as usize));
                     }
                     Tile::Obstruction => {
                         map.set_pos(ipos, Tile::Guard(rotate_dir(dir.clone())));
@@ -171,10 +229,12 @@ fn move_guard(map: &mut Map, pos: (usize, usize)) {
                 };
             } else {
                 map.set_pos(ipos, Tile::Empty(true));
+                return None;
             }
         }
         _ => unreachable!(),
     }
+    Some(pos)
 }
 
 fn rotate_dir(dir: Direction) -> Direction {
