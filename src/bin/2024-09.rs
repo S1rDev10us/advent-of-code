b0c7bdee8db17c2624a38bc25d45f906778ab6a3
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-11 12:07:05 +0000

    feat: 2024-09 star 2
    perf: 443.6 ms
    star 2 perf: 455.9 ms

--- src/bin/2024-09.rs
+++ src/bin/2024-09.rs
@@ -20,23 +20,24 @@ fn main() {
         .trim()
         .chars()
         .enumerate()
-        .flat_map(|(i, file_id)| {
-            let count = file_id.to_digit(10).unwrap();
-            vec![
+        .map(|(i, file_id)| {
+            let count = file_id.to_digit(10).unwrap() as usize;
+            (
+                count,
                 if i % 2 == 0 {
                     DiskBlock::File(i / 2)
                 } else {
                     DiskBlock::Free
-                };
-                count as usize
-            ]
+                },
+                false,
+            )
         })
         .collect::<Vec<_>>();
 
     // dbg!(&disk_map);
     let string_disk_block = &disk_map
         .iter()
-        .map(|block| format!("{}", block))
+        .map(|block| format!("{}", block.1).repeat(block.0))
         .collect::<Vec<_>>()
         .join("");
     if !actual_input {
@@ -46,25 +47,103 @@ fn main() {
         );
     }
 
-    while disk_map.contains(&DiskBlock::Free) && !disk_map.is_empty() {
-        let last_block = disk_map.pop().unwrap();
-        if matches!(last_block, DiskBlock::Free) {
+    while disk_map
+        .iter()
+        .any(|block| matches!(block, (1.., DiskBlock::Free, _)))
+        && !disk_map.is_empty()
+    {
+        // Remove empty blocks.
+        disk_map.retain(|block| matches!(block, (1.., _, _)));
+
+        // Combine adjacent empty blocks
+        let mut skip = false;
+        let mut offset = 0;
+        for (i, _) in disk_map.clone().iter().enumerate() {
+            let i = i - offset;
+            if i >= disk_map.len() - 1 {
+                continue;
+            }
+            if skip {
+                skip = false;
+                continue;
+            }
+            // If two adjacent blocks are free
+            if disk_map[i].1 == DiskBlock::Free && disk_map[i + 1].1 == DiskBlock::Free {
+                // Merge the free blocks
+                disk_map[i].0 += disk_map[i + 1].0;
+
+                disk_map.remove(i + 1);
+                skip = true;
+                offset += 1;
+            }
+        }
+
+        // Remove empty blocks at the end
+        while disk_map[disk_map.len() - 1].1 == DiskBlock::Free {
+            disk_map.pop();
+        }
+
+        if !actual_input {
+            println!(
+                "{}",
+                &disk_map
+                    .iter()
+                    .map(|block| "[".to_owned() + &format!("{}", block.1).repeat(block.0) + "]")
+                    .collect::<Vec<_>>()
+                    .join("")
+            );
+        }
+
+        // Get rightmost file block
+        let last_block_pos = disk_map.iter().rposition(|block| {
+            matches!(block, (1.., DiskBlock::File(_), _)) && !(star_2 && block.2)
+        });
+        if last_block_pos.is_none() {
+            break;
+        }
+        let last_block_pos = last_block_pos.unwrap();
+
+        disk_map[last_block_pos].2 = true;
+        let last_block = disk_map[last_block_pos];
+        let required_space = last_block.0;
+
+        // Get leftmost free block
+        let free_pos = disk_map.iter().position(|&block| {
+            matches!(block, (_, DiskBlock::Free, _)) && (!star_2 || block.0 >= required_space)
+        });
+        if free_pos.is_none() {
+            continue;
+        }
+        let free_pos = free_pos.unwrap();
+
+        let free_space = disk_map[free_pos].0;
+
+        if free_pos > last_block_pos {
             continue;
         }
 
-        let free_pos = disk_map
-            .iter()
-            .position(|&block| block == DiskBlock::Free)
-            .unwrap();
+        if !actual_input {
+            println!("{},{}", required_space, free_space);
+        }
 
-        disk_map[free_pos] = last_block;
+        if required_space > free_space {
+            // Overwrite empty space and decrease file block space
+            disk_map[free_pos].1 = last_block.1;
+            disk_map[free_pos].2 = true;
+            disk_map.get_mut(last_block_pos).unwrap().0 -= free_space;
+        } else {
+            disk_map[free_pos].0 -= required_space;
+            disk_map[last_block_pos].1 = DiskBlock::Free;
+            disk_map.insert(free_pos, last_block);
+        }
     }
     // dbg!(&disk_map);
 
     let output = disk_map
         .iter()
+        .flat_map(|block| vec![block.1; block.0])
         .enumerate()
-        .map(|(i, &block)| {
+        .map(|(i, block)| {
             if let DiskBlock::File(id) = block {
                 i * id
             } else {
@@ -79,7 +158,7 @@ fn main() {
         actual_input,
         Some(1928),          // star 1 test
         Some(6242766523059), // star 1 actual
-        None,                // star 2 test
-        None,                // star 2 actual
+        Some(2858),          // star 2 test
+        Some(6272188244509), // star 2 actual
     )
 }
