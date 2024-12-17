f8148b29ed0f8e7442a26805680c7616a52a1724
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-17 09:34:21 +0000

    fix: allow for custom duplicate merging function

--- src/merge.rs
+++ src/merge.rs
@@ -5,12 +5,18 @@ where
     T: PartialEq<T> + Ord,
     Ti: AddAssign<Ti> + Copy,
 {
+    merge_dupes_with(list, |a, b| b.1 += a.1);
+}
+pub fn merge_dupes_with<T: PartialEq<T> + Ord, To>(
+    list: &mut Vec<(T, To)>,
+    merger: impl Fn(&mut (T, To), &mut (T, To)),
+) {
     list.sort_by(|a, b| a.0.cmp(&b.0));
     list.dedup_by(|a, b| {
         if a.0 != b.0 {
             return false;
         }
-        b.1 += a.1;
+        merger(a, b);
         true
     })
 }
